dad624bea81e7f46b83a97cc9e848c312d80a72b
Yelsin1 <yelsinlous@gmail.com>
2022-12-24 06:39:10 +0100

    fixing map popups to campground

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,4 +8,10 @@ const map = new mapboxgl.Map({
 
 var marker = new mapboxgl.Marker()
         .setLngLat(geometry.coordinates)
+        .setPopup(
+            new mapboxgl.Popup({ offset: 25 })
+                .setHTML(
+                    `<h3>${nona}</h3>`
+                )
+        )
         .addTo(map);
